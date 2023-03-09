dcd52ec425ff2e493da4cf11f47571bb80770ffc
DzKy962 <127397723+DzKy962@users.noreply.github.com>
2023-03-09 18:50:51 +0700

    Add files via upload

---
+++ Kota.java
@@ -0,0 +1,19 @@
+ public class Kota<E> {
+     private E element;
+
+     public Kota(E Kota) {
+         element = Kota;
+     }
+
+     public E getElement() {
+         return element;
+     }
+
+     public static void main(String[] args) {
+         Kota<Integer> jumlahkota = new Kota<Integer>(9);
+         Kota<String> namakota = new Kota<String>("Malang");
+
+         System.out.println("Jumlah Kota di Jawa Timur: " + jumlahkota.getElement() + "kota");
+         System.out.println("Salah Satu Kota di Jawa Timur : Kota " + namakota.getElement());
+     }
+ }
